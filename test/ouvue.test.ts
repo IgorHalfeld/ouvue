7f4061c736ce2e8c0ed2206e0ba60623b5c00681
Igor Halfeld <hello@igorluiz.me>
2020-08-02 20:44:19 -0300

    100% of coverage :tada:

--- test/ouvue.test.ts
+++ test/ouvue.test.ts
@@ -17,7 +17,7 @@ interface Services {
   users: UserService
 }
 
-const user: User = { id: 444, name: 'Test' }
+let user: User = { id: 444, name: 'Test' }
 
 let instance: OuvueInstance
 const services: Services = {
@@ -27,7 +27,9 @@ const services: Services = {
       return [user].find(u => u.id === id) ?? {}
     },
     update: ({ name }) => {
-      user.name = name
+      // to test, needs to create another object to change the ref
+      const u = { ...user, name }
+      user = u
     },
     throwAnError() {
       throw new Error('throw an error')
@@ -44,7 +46,36 @@ describe('Ouvue', () => {
     expect(instance.fetch).toBeTruthy()
   })
 
-  it('should not broke when pass a unknown key', async () => {
+  it('should not break when not pass services', async () => {
+    const i = create<Services>({})
+    const response = await i.fetch<Services>('users/getAll', { name: 'Test getAll' })
+
+    expect(response).toEqual({
+      data: null,
+      error: { message: 'users/getAll not found on services object' }
+    })
+  })
+  it('should not break when not pass services', async () => {
+    const i = create<Services>({ cache: { strategy: 'inmemory' } })
+    const response = await i.fetch<Services>('users/getAll', { name: 'Test getAll' })
+
+    expect(response).toEqual({
+      data: null,
+      error: { message: 'users/getAll not found on services object' }
+    })
+  })
+
+  it('should not break when pass a unknown key', async () => {
+    const response = await instance.fetch<Services>('users/create', { name: 'Test create' })
+
+    expect(response).toEqual({
+      data: null,
+      error: { message: 'users/create not found on services object' }
+    })
+  })
+
+  it('should not broke when pass a unknown key and now show a warning when production', async () => {
+    process.env.NODE_ENV = 'production'
     const response = await instance.fetch<Services>('users/create', { name: 'Test create' })
 
     expect(response).toEqual({
@@ -74,8 +105,22 @@ describe('Ouvue', () => {
     expect(updateRes.error).toBeNull()
 
     const response2 = await instance.fetch<Services>('users/getById', { id: 444 })
-    expect(response2.data).toEqual({ name: 'Test 2', id: 444 })
+    expect(response2.data).toEqual({ name: 'Test', id: 444 })
     expect(response2.error).toBeNull()
+
+    // Use `onlyNetwork` to not check cache
+    const response3 = await instance.fetch<Services>(
+      'users/getById',
+      { id: 444 },
+      { onlyNetwork: true }
+    )
+    expect(response3.data).toEqual({ name: 'Test 2', id: 444 })
+    expect(response3.error).toBeNull()
+
+    // After use onlyNetwork, the cache was updated
+    const response4 = await instance.fetch<Services>('users/getById', { id: 444 })
+    expect(response4.data).toEqual({ name: 'Test 2', id: 444 })
+    expect(response4.error).toBeNull()
   })
 
   it('should throw an error if service fail', async () => {
