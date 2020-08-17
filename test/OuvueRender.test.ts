a4d7330e27ed9818bd64bdd8e81c36c093c0dc67
Igor Halfeld <hello@igorluiz.me>
2020-08-17 06:16:08 -0300

    tests for OuvueRender

--- test/OuvueRender.test.ts
+++ test/OuvueRender.test.ts
@@ -10,6 +10,7 @@ type User = {
 
 interface UserService {
   getAll(): Promise<User[]>
+  throwAnError(): Error
 }
 
 interface Services {
@@ -19,12 +20,11 @@ interface Services {
 let instance: OuvueInstance
 const services: Services = {
   users: {
-    getAll: () => Promise.resolve([{ id: 444, name: 'Test' }])
+    getAll: () => Promise.resolve([{ id: 444, name: 'Test' }]),
+    throwAnError: () => {
+      throw new Error('UserService - error on users service')
+    }
   }
-}
-
-const scopedSlots = {
-  default: '<span slot-scope="{ data }">{{ data }}</span>'
 }
 
 describe('OuvueRender', () => {
@@ -32,16 +32,37 @@ describe('OuvueRender', () => {
     instance = create<Services>({ services })
   })
 
-  it('should fetch data from entity/action', () => {
-    const wrapper = shallowMount(instance.OuvueRender, {
+  it('should fetch data from entity/action', async () => {
+    const wrapper = shallowMount<Vue & { fetch(): Promise<void> }>(instance.OuvueRender, {
       propsData: {
         action: 'users/getAll'
       },
-      scopedSlots
+      scopedSlots: {
+        default: '<pre slot-scope="{ data }">{{ data }}</pre>'
+      }
     })
 
-    wrapper.vm.$nextTick(() => {
-      expect(wrapper.text()).toBe('{ id: 444, name: "Test" }')
+    await wrapper.vm.fetch()
+
+    const text = JSON.parse(wrapper.text())
+    expect(text).toEqual([{ id: 444, name: 'Test' }])
+  })
+
+  it('should return a error from entity/action', async () => {
+    const wrapper = shallowMount<Vue & { fetch(): Promise<void> }>(instance.OuvueRender, {
+      propsData: {
+        action: 'users/throwAnError'
+      },
+      scopedSlots: {
+        default: '<pre slot-scope="{ error }">{{ error }}</pre>'
+      }
+    })
+
+    await wrapper.vm.fetch()
+
+    const text = JSON.parse(wrapper.text())
+    expect(text).toEqual({
+      message: 'some error happen on request call'
     })
   })
 })
